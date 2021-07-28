401c61624b1a1cef45729f9ec52311fc13cf49a2
Raúl Dueñas <raulduenas29@gmail.com>
2021-07-28 12:02:17 -0500

    bases 2

---
+++ src/app/dbz/main-page/main-page.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+import { DbzService } from '../services/dbz.service';
+
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html',
+  styleUrls: ['./main-page.component.css']
+})
+export class MainPageComponent {
+
+  // get personajes(): Personaje[] {
+  //   return this.dbzService.personajes;
+  // }
+
+  nuevo: Personaje = {
+    nombre: "Maestro Roshi",
+    poder : 1000
+  }
+
+  // agregarNuevoPersonaje(argumento: Personaje) {
+  //   this.personajes.push(argumento);
+  // }
+
+  constructor( private dbzService: DbzService ) {}
+
+}
